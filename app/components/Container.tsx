680019969705fa4ca55aeebac91d138a3890d486
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-12-15 19:18:35 +0530

    Navbar UI done

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({children}) => {
     return (
-        <div className={"max-w-[1080px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4"}>
+        <div className={"w-full mx-auto xl:px-20 md:px-10 sm:px-2 px-4"}>
             {children}
         </div>
     );
