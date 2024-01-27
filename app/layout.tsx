8ccff54c0df3cc7e8eed527a26e9f2ce5090ddfc
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-01-27 20:56:26 +0530

    Made and implemented toaster provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/app/components/navbar/Navbar";
 import React from "react";
 import Modal from "@/app/components/modals/modal";
 import RegisterModal from "@/app/components/modals/RegisterModal";
+import ToasterProvider from "@/app/providers/ToasterProvider";
 
 const font = Nunito({
     subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({
         return (
             <html lang="en">
                 <body className={font.className}>
+                    <ToasterProvider />
                     <RegisterModal />
                     <Navbar />
                     {children}
