0157145a41be4477122ab9c2e296992be1390875
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-01-30 23:34:54 +0530

    Added login modal to open from the user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,12 @@ import {AiOutlineMenu} from "react-icons/ai";
 import Avatar from "@/app/components/Avatar";
 import MenuItem from "@/app/components/navbar/MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const UserMenu = () => {
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
@@ -39,7 +42,7 @@ const UserMenu = () => {
                 <div className={"absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-o top-12 text-sm"}>
                     <div className="flex flex-col cursor-pointer">
                         <>
-                            <MenuItem onClick={() => {}} label={"Login"} />
+                            <MenuItem onClick={loginModal.onOpen} label={"Login"} />
                             <MenuItem onClick={registerModal.onOpen} label={"Sign Up"} />
                         </>
                     </div>
