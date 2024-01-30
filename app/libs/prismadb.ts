26e3d843c6dc9688da5acbf92a3f10a8de82d1b8
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-01-30 23:21:46 +0530

    Added conditional check to avoid generating new prisma clients while hot reloading

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+	let prisma: PrismaClient | undefined;
+}
+
+
+// @ts-ignore
+const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+	// @ts-ignore
+	globalThis.prisma = client;
+}
+
+
+export default client;
