b673751985f15ef5582333aebd8aaff09230b6aa
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-01 21:17:22 +0530

    Added currentUser prop

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,27 +1,35 @@
 "use client";
 
+import React from "react";
+
 import Container from "@/app/components/Container";
 import Logo from "@/app/components/navbar/Logo";
 import Search from "@/app/components/navbar/Search";
+
 import UserMenu from "@/app/components/navbar/UserMenu";
+import { User } from "@prisma/client";
 
-const Navbar = () => {
-    return (
-        <div className={"fixed w-full bg-white z-10 shadow-sm"}>
-            <div className={"py-4 border-b[1px]"}>
+interface NavbarProps {
+	currentUser?: User | null;
+}
 
-                {/*Passes whatever I write here as children prop to Container.tsx*/}
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+	return (
+		<div className={ "fixed w-full bg-white z-10 shadow-sm" }>
+			<div className={ "py-4 border-b[1px]" }>
 
-                <Container>
-                    <div className={"flex flex-row items-center justify-between gap-3 md:gap-0"}>
-                        <Logo />
-                        <Search />
-                        <UserMenu />
-                    </div>
-                </Container>
-            </div>
-        </div>
-    );
+				{/*Passes whatever I write here as children prop to Container.tsx*/ }
+
+				<Container>
+					<div className={ "flex flex-row items-center justify-between gap-3 md:gap-0" }>
+						<Logo/>
+						<Search/>
+						<UserMenu currentUser={currentUser}/>
+					</div>
+				</Container>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
