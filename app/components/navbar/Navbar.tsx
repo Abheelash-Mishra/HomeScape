8553fe62f8e64c4982c0c4e7f89a20e63b4e150b
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-02 00:13:58 +0530

    Changed to the safe User type

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,10 +7,10 @@ import Logo from "@/app/components/navbar/Logo";
 import Search from "@/app/components/navbar/Search";
 
 import UserMenu from "@/app/components/navbar/UserMenu";
-import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-	currentUser?: User | null;
+	currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
