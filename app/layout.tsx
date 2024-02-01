581db6ce7651715894d226d9094fb87100e0f366
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-01 21:18:15 +0530

    Added currentUser prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Modal from "@/app/components/modals/modal";
 import RegisterModal from "@/app/components/modals/RegisterModal";
 import ToasterProvider from "@/app/providers/ToasterProvider";
 import LoginModal from "@/app/components/modals/LoginModal";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 const font = Nunito({
     subsets: ['latin'],
@@ -16,18 +17,19 @@ export const metadata = {
     description: 'Book Your Dream Getaway',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
     }: {
         children: React.ReactNode
     }) {
+    const currentUser = await getCurrentUser()
         return (
             <html lang="en">
                 <body className={font.className}>
                     <ToasterProvider />
                     <LoginModal />
                     <RegisterModal />
-                    <Navbar />
+                    <Navbar currentUser={currentUser} />
                     {children}
                 </body>
             </html>
