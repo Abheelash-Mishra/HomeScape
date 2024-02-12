15a380304f7374e0fe0961792d630d71548666c7
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-12 20:12:47 +0530

    Implemented listing cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
 import Image from 'next/image'
+import getListings from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export default function Home() {
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+export default async function Home() {
+    const listings = await getListings();
+    const currentUser = await getCurrentUser();
+
+    if (listings.length === 0) {
+        return (
+            <EmptyState showReset/>
+        )
+    }
+
     return (
-        <div className={"text-row-500 text-2xl"}>
-        </div>
+        <Container>
+            <div className={ "pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8" }>
+                {listings.map((listing: any) => {
+                    return (
+                        <ListingCard
+                            currentUser={currentUser}
+                            key={listing.id}
+                            data={listing}
+                        />
+                    )
+                })}
+
+            </div>
+        </Container>
     )
 }
