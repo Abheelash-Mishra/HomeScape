049a58f50ff49e02d1620b5a97dc3e2b5f087456
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-12 18:29:33 +0530

    Implemented favoriting

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -3,23 +3,23 @@
 import React from "react";
 import { SafeUser } from "@/app/types";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavorite from "@/app/hooks/useFavorite";
 
 interface HeartButtonProps {
 	listingId: string;
 	currentUser?: SafeUser | null;
 }
 
-const HeartButton: React.FC<HeartButtonProps> = ({listingId, currentUser}) => {
-	const hasFavorited = false;
-	const toggleFavorite = () => {}
+const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => {
+	const { hasFavorited, toggleFavorite } = useFavorite({ listingId, currentUser });
 
 	return (
 		<div
-			onClick={toggleFavorite}
-			className={"relative hover:opacity-80 transition cursor-pointer"}
+			onClick={ toggleFavorite }
+			className={ "relative hover:opacity-80 transition cursor-pointer" }
 		>
-			<AiOutlineHeart size={28} className={"fill-white absolute -top-[2px] -right-[2px]"} />
-			<AiFillHeart size={24} className={hasFavorited ? "fill-rose-500" : "fill-neutral-500"} />
+			<AiOutlineHeart size={ 28 } className={ "fill-white absolute -top-[2px] -right-[2px]" }/>
+			<AiFillHeart size={ 24 } className={ hasFavorited ? "fill-red-600" : "fill-neutral-500" }/>
 		</div>
 	);
 };
