dcb2613fd626503ef453d1ac563cd06c89934ce2
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-02-13 17:37:31 +0530

    Fixed issue by changing let to var

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-	let prisma: PrismaClient | undefined;
+	var prisma: PrismaClient | undefined;
 }
 
-
-// @ts-ignore
 const client = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-	// @ts-ignore
 	globalThis.prisma = client;
 }
 
